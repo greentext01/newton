94826e713a327e8d042b2d9182875f04844d3a81
Olivier Audet-Yang <oaudetyang@gmail.com>
2023-07-08 00:11:32 -0400

    Added window icon + script to generate it

--- flight/src/main.rs
+++ flight/src/main.rs
@@ -1,5 +1,6 @@
 mod cli;
 mod networking;
+mod graphics;
 
 use std::{
     sync::{
@@ -10,19 +11,39 @@ use std::{
 };
 
 use common::data::state::State;
-use macroquad::prelude::*;
+use env_logger::Env;
+use graphics::icon::*;
+use macroquad::{miniquad::conf::Icon, prelude::*};
 use networking::client::{Client, Config};
 
-#[macroquad::main("Flight")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Flight".to_owned(),
+        window_width: 800,
+        window_height: 600,
+        icon: Some(Icon {
+            small: ICON_SMALL,
+            medium: ICON_MEDIUM,
+            big: ICON_BIG,
+        }),
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
-    env_logger::init();
+    let env = Env::default()
+        .filter_or("FLIGHT_LOG_LEVEL", "info")
+        .write_style_or("FLIGHT_LOG_STYLE", "always");
+
+    env_logger::init_from_env(env);
 
     let state_lock: Arc<RwLock<Option<State>>> = Arc::new(RwLock::new(None));
 
     let quit_flag = Arc::new(AtomicBool::new(false));
 
     let client_config = Config {
-        network_interface: "",
+        network_interface: "127.0.0.1",
         network_port: 5000,
     };
 
@@ -39,17 +60,37 @@ async fn main() {
     let client = client.unwrap();
 
     thread::spawn(move || {
-        log::info!("Network thread starting...");
+        log::trace!("Network thread starting...");
 
         client.run();
     });
 
     loop {
-        clear_background(BLACK);
-
         if quit_flag.load(Ordering::SeqCst) {
             log::trace!("Quitting from graphics thread...");
             break;
+        }
+
+        let state_guard = state_lock.read().unwrap();
+        let state = state_guard.clone();
+        drop(state_guard);
+
+        if state.is_none() {
+            next_frame().await;
+            continue;
+        }
+
+        let state = state.unwrap();
+
+        clear_background(BLACK);
+
+        for planet in state.planets.iter() {
+            draw_circle(
+                planet.object.position[0] as f32,
+                planet.object.position[1] as f32,
+                planet.radius as f32,
+                WHITE,
+            );
         }
 
         next_frame().await;
